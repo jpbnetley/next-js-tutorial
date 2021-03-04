8c14efeb76fcaa9b74fbea7ae9dc29dccf7cd886
Jonathan Netley <jnetley@fullfacing.com>
2021-03-04 08:17:20 +0200

    added description text to index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,13 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis porttitor enim non consectetur.
+          Proin venenatis, mi nec tristique vehicula, lectus turpis gravida lacus, vel fringilla risus urna semper turpis. 
+          Curabitur commodo consectetur lacus, vitae semper nisl mollis vitae. Mauris dolor erat, condimentum eu tellus ac, 
+          faucibus commodo sapien. In tellus dui, fermentum id auctor consectetur, suscipit ut orci. In in consequat neque. 
+          Donec nulla mauris, porta quis arcu at, consequat cursus diam. Proin ligula magna, posuere eget pharetra sollicitudin, 
+          facilisis vitae lorem. Sed interdum dolor tempor enim lobortis finibus. Proin non blandit massa. Phasellus venenatis 
+          imperdiet feugiat.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
